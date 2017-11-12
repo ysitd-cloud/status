a6deeb7fd54662f9294d2a68fdbaeec49e2b8a53
Tony Yip <tony@opensource.hk>
2017-11-13 01:52:41 +0800

    Use k8s utils

--- http/handlers/components/endpoints.go
+++ http/handlers/components/endpoints.go
@@ -4,44 +4,17 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/pkg/api/v1"
+	"github.com/ysitd-cloud/k8s-utils/services"
 )
-
-func createPodInfo(addresses []v1.EndpointAddress) *podInfo {
-	names := make([]string, 0)
-	for _, pod := range addresses {
-		names = append(names, pod.TargetRef.Name)
-	}
-
-	return &podInfo{
-		Total: len(addresses),
-		Pods:  names,
-	}
-}
 
 func createComponentStatusEndpoint(name string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		endpoint, err := coreV1.Endpoints(namespace).Get(name, metav1.GetOptions{})
+		resp, err := services.GetServiceStatus(coreV1, namespace, name)
 		if err != nil {
 			c.AbortWithError(http.StatusInternalServerError, err)
-			return
+		} else {
+			c.JSON(http.StatusOK, &resp)
+			c.Abort()
 		}
-
-		subset := endpoint.Subsets[0]
-
-		notReady := subset.NotReadyAddresses
-		notReadyPods := createPodInfo(notReady)
-
-		ready := subset.Addresses
-		readyPods := createPodInfo(ready)
-
-		resp := componentEndpoints{
-			Total:        len(ready) + len(notReady),
-			Available:    readyPods,
-			NotAvailable: notReadyPods,
-		}
-
-		c.JSON(http.StatusOK, &resp)
 	}
 }
--- http/handlers/components/param.go
+++ http/handlers/components/param.go
@@ -1,40 +1,22 @@
 package components
 
 import (
-	"path/filepath"
-
 	"os"
 
+	"github.com/ysitd-cloud/k8s-utils"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/typed/core/v1"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
 )
 
 var clientSet *kubernetes.Clientset
 var coreV1 v1.CoreV1Interface
-var namespace string = os.Getenv("SERVICE_NAMESPACE")
+var namespace = os.Getenv("SERVICE_NAMESPACE")
 
 func init() {
-	var config *rest.Config
 	var err error
-	config, err = loadInOfClusterConfig()
+	clientSet, err = utils.AutoConnect()
 	if err != nil {
-		config, err = loadOutOfClusterConfig()
-		if err != nil {
-			panic(err)
-		}
+		panic(err)
 	}
-
-	clientSet = kubernetes.NewForConfigOrDie(config)
 	coreV1 = clientSet.CoreV1()
-}
-
-func loadInOfClusterConfig() (config *rest.Config, err error) {
-	return rest.InClusterConfig()
-}
-
-func loadOutOfClusterConfig() (config *rest.Config, err error) {
-	file := filepath.Join(os.Getenv("HOME"), ".kube", "config")
-	return clientcmd.BuildConfigFromFlags("", file)
 }
