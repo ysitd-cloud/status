e8b9fab83fb004cb0c472bde78ae1c3d9638ac49
Tony Yip <tony@opensource.hk>
2017-12-11 01:56:26 +0800

    Add component for status

--- http/handlers/components/register.go
+++ http/handlers/components/register.go
@@ -5,6 +5,7 @@ import "github.com/gin-gonic/gin"
 var serviceMap = map[string]string{
 	"account": "account-manager",
 	"status": "status-api",
+	"controller": "app-controller",
 }
 
 func Register(group *gin.RouterGroup) {
